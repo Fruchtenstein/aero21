f58369d42f26362218463c59928805a68b022b3d
Dimitri Fruchtenstein <fruchtenstein@gmail.com>
2021-01-20 18:40:44 +0000

    change path for mini-maps to html/maps

--- map.rb
+++ map.rb
@@ -8,11 +8,11 @@ db = Mysql2::Client.new(:host => "localhost", :username => DBUSER, :password => 
 pls = db.query("SELECT runid, summary_polyline FROM log", :as => :array).to_a
 pls.each do |p|
   puts "=== #{p[0]}"
-  unless File.exists?("html/m#{p[0]}.png")
+  unless File.exists?("html/maps/m#{p[0]}.png")
     url = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/static/path-3+f44-0.5(#{ERB::Util.url_encode(p[1])})/auto/300x300?access_token=#{MAPBOX_TOKEN}"
     c = HTTPClient.new
     r = c.get(url)
-    File.open("html/m#{p[0]}.png", 'w') do |f|
+    File.open("html/maps/m#{p[0]}.png", 'w') do |f|
       f.write(r.body)
     end
     puts "+++ m#{p[0]}.png created"
